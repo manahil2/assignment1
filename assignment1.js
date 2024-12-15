f9457900699b81eeb3cbeb2a0b487d92843ae00b
Manahil Aleem <manahilaleem1@gmail.com>
2024-12-15 17:04:18 +0500

    deploy

---
+++ assignment1.js
@@ -0,0 +1,14 @@
+//1
+alert("Welcome to Our Website!We hope you have a great experience");
+
+//2
+alert("Error! Please enter a valid password.");
+
+//3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+//4
+alert("Welcome to Js Land...");
+alert("HAppy Coding!");
+
+
